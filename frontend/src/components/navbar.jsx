fbd9edfbedab14b640c181367b6d3e4643f61cb6
ratnakarguru <ratnakarguru16@gmail.com>
2026-06-06 09:29:37 +0530

    Added better navbar

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 function Navbar({ user }) {
   const navigate = useNavigate();
