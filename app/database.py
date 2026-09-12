f17c01dffbea0c07d1de16f779027482529198fd
ratnakarguru <ratnakarguru16@gmail.com>
2026-09-12 08:29:14 +0530

    Added new part

--- app/database.py
+++ app/database.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = "postgresql://neondb_owner:npg_9s5mnApqaCeb@ep-calm-wind-b3wqkj08-pooler.c-4.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
 
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL environment variable is not set")
